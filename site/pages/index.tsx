624316d3ecf6930522a16f00557b05be1782ed78
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-10-03 20:37:58 +0800

    wip

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -144,7 +144,7 @@ const Index: React.FC = (): React.ReactElement => {
 
             <section className="tw-my-32 tw-mx-5 lg:tw-mx-40 tw-text-center">
                 <h1 className="tw-font-bold tw-text-2xl tw-mb-4">
-                    {/* Featured Projects */}
+                    Featured Projects
                 </h1>
 
                 <div className="tw-flex tw-flex-wrap">
