c324d11890ade32ab38c54dd5df739e784dff911
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-10-09 13:23:32 +0800

    Updated test step in api workflow

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $APP/.env .github/secrets/$APP.env.gpg
