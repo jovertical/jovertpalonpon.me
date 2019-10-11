fd67d0aa2733e416ef5422b2c729ebc2d7a84b51
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-10-11 12:43:16 +0800

    Fix for TypeORM issues interferring tests

--- api/src/tests/http/app.spec.ts
+++ api/src/tests/http/app.spec.ts
@@ -1,5 +1,13 @@
 import * as request from 'supertest'
+import { createConnection } from 'typeorm'
 import app from '../../bootstrap'
+import { getDatabaseConnection } from '../../helpers'
+
+beforeAll(async () => {
+    // This is used to avoid time-out errors because the bootstrap file
+    // may contain database connection attempts which is synchronous
+    await createConnection(getDatabaseConnection())
+})
 
 describe('Application Endpoints', () => {
     it('should print the welcome message', async () => {
