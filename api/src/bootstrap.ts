8041cf9adef490aaeeb8bb868d07423a6e911a1a
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-12-16 01:52:25 +0800

    Add default CORS_ORIGIN in bootstrap file

--- api/src/bootstrap.ts
+++ api/src/bootstrap.ts
@@ -27,7 +27,9 @@ const server = new InversifyExpressServer(container)
 // Configure express
 server.setConfig((app: express.Application) => {
   app.use(bodyParser.json())
-  app.use(cors({ origin: process.env.CORS_ORIGIN }))
+  app.use(
+    cors({ origin: process.env.CORS_ORIGIN || 'https://jovertpalonpon.me' })
+  )
 })
 
 const app = server.build()
