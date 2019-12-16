333adebe7749590c7e9d614c91de249d7f759394
Jovert Lota Palonpon <palonponjovertlota@gmail.com>
2019-12-16 23:33:19 +0800

    Feat/projects pre rendering (#9)
    * Fetch projects when exporting
    
    * Remove links in Header

--- site/next.config.js
+++ site/next.config.js
@@ -1,17 +1,35 @@
 const path = require('path')
+const fetch = require('isomorphic-unfetch')
 const withCss = require('@zeit/next-css')
 const withPurgeCss = require('next-purgecss')
 
 const config = {
+  exportTrailingSlash: true,
+  exportPathMap: async function () {
+    const paths = {
+      '/': { page: '/' },
+    }
+
+    const res = await fetch(this.env.API_URL + '/projects')
+    const projects = await res.json()
+
+    projects.forEach(({ slug }) => {
+      paths[`/projects/${slug}`] = {
+        page: '/projects/[slug]', query: { slug }
+      }
+    })
+
+    return paths
+  },
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:8080'
+  },
   webpack(config) {
     config.resolve.alias['@components'] = path.join(__dirname, 'components')
     config.resolve.alias['@constants'] = path.join(__dirname, 'constants')
     config.resolve.alias['@helpers'] = path.join(__dirname, 'helpers')
     config.resolve.alias['@styles'] = path.join(__dirname, 'styles')
     return config
-  },
-  env: {
-    API_URL: process.env.API_URL || 'http://localhost:8080'
   },
 }
 
