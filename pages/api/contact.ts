6ef9e14d22b8711d606a2d300bd918b71e7fd52d
Jovert Palonpon <palonponjovertlota@gmail.com>
2020-01-18 15:21:40 +0800

    chore: Contacts API

---
+++ pages/api/contact.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { check } from 'express-validator'
+import validate from '@backend/middlewares/validate'
+import Mailer from '@backend/services/mailer'
+
+export default validate(
+  [
+    check('name').notEmpty(),
+    check('email')
+      .notEmpty()
+      .isEmail(),
+    check('message').notEmpty()
+  ],
+  async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    Mailer.send({
+      to: 'palonponjovertlota@gmail.com',
+      from: req.body?.email,
+      subject: req.body?.name + ' wants to talk to you',
+      text: req.body?.message
+    })
+
+    res.send('Sending message...')
+  }
+)
